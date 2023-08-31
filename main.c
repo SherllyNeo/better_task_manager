6ff8fef039e2d68a0bfd79b0f5da58d23461d0bf
sherllyneo <sherlly.neomail@gmail.com>
2023-08-31 19:48:12 +0100

    added error handling for removal

--- main.c
+++ main.c
@@ -59,7 +59,7 @@ int main(int argc, char** argv) {
             printf("\nTask removed!\n");
         }
         else{
-            fprintf(stderr,"\nError removing task!\n");
+            fprintf(stderr,"Error removing task!\n");
         }
     }
     else {
